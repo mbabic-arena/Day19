71a04a2204f2856657cadf070640bfd3a627ff7e
mbabic-arena <m_babic@teol.net>
2021-03-26 14:54:48 +0100

    ex04

---
+++ ex04/src/index.js
@@ -0,0 +1,17 @@
+// src/index.js
+
+import React, { Component } from "react"; // Change this line
+import ReactDOM, { render } from "react-dom";
+import "./index.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello, React!</h1>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
